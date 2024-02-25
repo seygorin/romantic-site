7176d2416267abb7c9f29c5035e68ebbd4c9fd6d
Sergey Gorin <seygorin@gmail.com>
2024-02-25 15:59:20 +0500

    fix: lint errors at v2 02/25/2024 15:59:20

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,3 @@
-// eslint-disable-next-line
 import { AppProps } from 'next/app';
 import Layout from '@/widgets/layouts';
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,2 +1,1 @@
-// eslint-disable-next-line
 export { Main as default } from '@/pages/main/ui';
